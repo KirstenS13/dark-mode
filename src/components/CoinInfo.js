d9178a820212a185c7d7de76c27feaf8f0107d22
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-06-09 21:45:34 -0400

    started on stretch goals, added routing, added HomePage.js and CoinInfo.js, styled the homepage and navbar

---
+++ src/components/CoinInfo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CoinInfo = () => {
+    return (
+        <div>
+            <p>Coin Info</p>
+        </div>
+    )
+}
+
+export default CoinInfo;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
+import HomePage from "./components/HomePage";
 
 import "./styles.scss";
 
@@ -15,16 +17,27 @@ const App = () => {
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
-      .then(res => setCoinData(res.data))
+      .then(res => {
+        setCoinData(res.data);
+        console.log('res', res);
+      })
       .catch(err => console.log(err));
   }, []);
   return (
     <div className="App">
       <Navbar />
-      <Charts coinData={coinData} />
+      {/* <HomePage /> */}
+      {/* <Charts coinData={coinData} /> */}
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+        <Route path="/charts" render={(props) => <Charts {...props} coinData={coinData} />} />
+      </Switch>
     </div>
   );
 };
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>, rootElement);
